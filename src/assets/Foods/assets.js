aae6b054cb92ca43b7304996ac46b301c07f85cb
Lexicon <iyiolad21@gmail.com>
2024-09-21 21:49:14 +0100

    My Food delivery app first commit

---
+++ src/assets/Foods/assets.js
@@ -0,0 +1,350 @@
+import basket_icon from './basket_icon.png'
+import logo from 'https://res.cloudinary.com/dtlejpoxq/image/upload/v1723417677/Screenshot_12-8-2024_0159_looka.com-removebg-preview_2_lmglnf.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './menu1.jpeg'
+import menu_2 from './menu2.jpeg'
+import menu_3 from './menu3.jpeg'
+import menu_4 from './menu4.jpeg'
+import menu_5 from './menu5.jpeg'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+
+import food_1 from './Food1.jpeg'
+import food_2 from './Food2.jpeg'
+import food_3 from './Food3.jpeg'
+import food_4 from './Food4.jpeg'
+import food_5 from './Food5.jpeg'
+import food_6 from './Food6.jpeg'
+import food_7 from './Food7.jpeg'
+import food_8 from './Food8.jpeg'
+import food_9 from './Food9.jpeg'
+import food_10 from './Food10.jpeg'
+import food_11 from './Food11.jpeg'
+import food_12 from './Food12.jpeg'
+import food_13 from './Food13.jpeg'
+import food_14 from './Food14.jpeg'
+import food_15 from './Food15.jpeg'
+import food_16 from './Food16.jpeg'
+import food_17 from './Food17.jpeg'
+import food_18 from './Food18.jpeg'
+import food_19 from './Food19.jpeg'
+import food_20 from './Food20.jpeg'
+import food_21 from './Food21.jpeg'
+import food_22 from './Food22.png'
+import food_23 from './Food23.png'
+import food_24 from './Food24.png'
+import food_25 from './Food25.png'
+import food_26 from './Food26.png'
+import food_27 from './Food27.png'
+import food_28 from './Food28.png'
+import food_29 from './Food29.png'
+import food_30 from './Food30.png'
+import food_31 from './Food31.png'
+import food_32 from './Food32.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Rice",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Meat",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Native",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Mixed",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Special Food",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Pure Veg",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Pasta",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Noodles",
+        menu_image: menu_8
+    }]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Jollof Rice",
+        image: food_1,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Rice"
+    },
+    {
+        _id: "2",
+        name: "Fried Rice",
+        image: food_2,
+        price: 18,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Rice"
+    }, {
+        _id: "3",
+        name: "Amala",
+        image: food_3,
+        price: 16,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Native"
+    }, {
+        _id: "4",
+        name: "Asun",
+        image: food_4,
+        price: 24,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Meat"
+    }, {
+        _id: "5",
+        name: "Suya",
+        image: food_5,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Meat"
+    }, {
+        _id: "6",
+        name: "Porridge",
+        image: food_6,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Mixed"
+    }, {
+        _id: "7",
+        name: "Pounded Yam",
+        image: food_7,
+        price: 20,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Native"
+    }, {
+        _id: "8",
+        name: "Efo Riro",
+        image: food_8,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Native"
+    }, {
+        _id: "9",
+        name: "Ofada Rice",
+        image: food_9,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Rice"
+    }, {
+        _id: "10",
+        name: "Stach and Banga Soup",
+        image: food_10,
+        price: 22,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Native"
+    }, {
+        _id: "11",
+        name: "Jollof Rice with Croaker Fish",
+        image: food_11,
+        price: 10,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Rice"
+    }, {
+        _id: "12",
+        name: "Gizz Dodo",
+        image: food_12,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Meat"
+    },
+    {
+        _id: "13",
+        name: "Ewa Agoyin",
+        image: food_13,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Mixed"
+    },
+    {
+        _id: "14",
+        name: "Fried rice1",
+        image: food_14,
+        price: 18,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Rice"
+    }, {
+        _id: "15",
+        name: "Fufu",
+        image: food_15,
+        price: 16,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Native"
+    }, {
+        _id: "16",
+        name: "Yam and egg",
+        image: food_16,
+        price: 24,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Special Food"
+    }, {
+        _id: "17",
+        name: "Eko and Efo",
+        image: food_17,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Native"
+    }, {
+        _id: "18",
+        name: "Small Chops",
+        image: food_18,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Special Food"
+    }, {
+        _id: "19",
+        name: "Plantain and egg",
+        image: food_19,
+        price: 20,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Special Food"
+    }, {
+        _id: "20",
+        name: "Moin Moin",
+        image: food_20,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Native" 
+    }, {
+        _id: "21",
+        name: "Eba and Okro",
+        image: food_21,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Native"
+    }, {
+        _id: "22",
+        name: "Fried Cauliflower",
+        image: food_22,
+        price: 22,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Pure Veg"
+    }, {
+        _id: "23",
+        name: "Mix Veg Pulao",
+        image: food_23,
+        price: 10,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Pure Veg"
+    }, {
+        _id: "24",
+        name: "Rice Zucchini",
+        image: food_24,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Pure Veg"
+    },
+    {
+        _id: "25",
+        name: "Cheese Pasta",
+        image: food_25,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Pasta"
+    },
+    {
+        _id: "26",
+        name: "Tomato Pasta",
+        image: food_26,
+        price: 18,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Pasta"
+    }, {
+        _id: "27",
+        name: "Creamy Pasta",
+        image: food_27,
+        price: 16,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Pasta"
+    }, {
+        _id: "28",
+        name: "Chicken Pasta",
+        image: food_28,
+        price: 24,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Pasta"
+    }, {
+        _id: "29",
+        name: "Buttter Noodles",
+        image: food_29,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Noodles"
+    }, {
+        _id: "30",
+        name: "Veg Noodles",
+        image: food_30,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Noodles"
+    }, {
+        _id: "31",
+        name: "Somen Noodles",
+        image: food_31,
+        price: 20,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Noodles"
+    }, {
+        _id: "32",
+        name: "Cooked Noodles",
+        image: food_32,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Noodles"
+    }
+]
---
+++ src/assets/admin_assets/assets.js
@@ -0,0 +1,17 @@
+import logo from 'https://res.cloudinary.com/dtlejpoxq/image/upload/v1723417677/Screenshot_12-8-2024_0159_looka.com-removebg-preview_2_lmglnf.png'
+import add_icon from './add_icon.png'
+import order_icon from './order_icon.png'
+import profile_image from './profile_image.png'
+import upload_area from './upload_area.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets ={
+    logo,
+    add_icon,
+    order_icon,
+    profile_image,
+    upload_area,
+    parcel_icon
+}
+
+export const url = 'http://localhost:4000'
